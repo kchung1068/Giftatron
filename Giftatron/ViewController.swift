13743a2bacb505eceb9ef5d8c9cf9f163f063e0d
Kyle Chung <kchung1068@stu.d214.org>
2019-12-04 13:39:11 -0600

    Initial Commit

---
+++ Giftatron/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Giftatron
+//
+//  Created by Kyle C on 12/4/19.
+//  Copyright © 2019 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
