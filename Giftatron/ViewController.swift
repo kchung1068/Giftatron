439e7561b625d743c7970b454bd7bd6f35c1d076
Kyle Chung <kchung1068@stu.d214.org>
2019-12-04 13:51:00 -0600

    Added Giftatron Label

--- Giftatron/ViewController.swift
+++ Giftatron/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var gifttronLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        gifttronLabel.textColor = .green
+      
     }
 
 
