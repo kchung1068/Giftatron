c57601b4522dd706bc8efb79c8544b79f411e0fe
Kyle Chung <kchung1068@stu.d214.org>
2019-12-05 13:48:03 -0600

    Added image

--- Giftatron/ViewController.swift
+++ Giftatron/ViewController.swift
@@ -14,23 +14,29 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var textField: UITextField!
     
+    var friendName = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         gifttronLabel.textColor = .green
       
     }
     @IBAction func totheQuiz(_ sender: Any) {
-    func getProducts(url: String) {
-        let url = URL(string: url)!
-                URLSession.shared.dataTask(with: url) { (data, response, error) in
-                    if let data = data {
-                        let jsonObject = try? JSONSerialization.jsonObject(with:data,options:JSONSerialization.ReadingOptions.allowFragments)
-                    }
+        if let friendName = textField.text {
+            self.friendName = friendName
+            print(friendName)
         }
-        
      
     }
     
+    public func getProducts(url: String) {
+           let url = URL(string: url)!
+                   URLSession.shared.dataTask(with: url) { (data, response, error) in
+                       if let data = data {
+                           let jsonObject = try? JSONSerialization.jsonObject(with:data,options:JSONSerialization.ReadingOptions.allowFragments)
+                       }
+           }
 
 }
 
+}
