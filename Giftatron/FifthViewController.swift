b08fc36841362748295babafd66b884b78060991
Kyle Chung <kchung1068@stu.d214.org>
2020-03-11 13:48:43 -0500

    Segue

---
+++ Giftatron/FifthViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FifthViewController.swift
+//  Giftatron
+//
+//  Created by Kyle C on 3/11/20.
+//  Copyright © 2020 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class FifthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
