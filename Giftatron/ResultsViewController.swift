b89697d46ef222a13e6afed4c54a98c72fd4dc95
Emmett Hasley <Ehasley1136@stu.d214.org>
2020-03-12 13:55:46 -0500

    Added refresh boy

--- Giftatron/ResultsViewController.swift
+++ Giftatron/ResultsViewController.swift
@@ -125,25 +125,25 @@ class ResultsViewController: UIViewController, UITableViewDataSource,UITableView
         }
     }
     func addNewItem() {
-        let alert = UIAlertController(title: "New Item", message: "enter your item name below", preferredStyle: .alert)
-        alert.addTextField { (TextField) in
-            TextField.placeholder = "Price"
-        }
-        alert.addTextField { (TextField2) in
-            TextField2.placeholder = "Name"
-        }
-        alert.addTextField { (TextField3) in
-        }
-        let OK = UIAlertAction(title: "Go", style: .destructive) { (OK) in
-            let item = Product(image: UIImage(), price: Double(alert.textFields![0].text!)!, name: alert.textFields![1].text!, url: alert.textFields![2].text!)
-            //            self.arrayOfProducts.append(item)
-            self.resultTableView.reloadData()
-        }
-        let cancel = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
-        
-        alert.addAction(OK)
-        alert.addAction(cancel)
-        present(alert, animated: true, completion: nil)
+//        let alert = UIAlertController(title: "New Item", message: "enter your item name below", preferredStyle: .alert)
+//        alert.addTextField { (TextField) in
+//            TextField.placeholder = "Price"
+//        }
+//        alert.addTextField { (TextField2) in
+//            TextField2.placeholder = "Name"
+//        }
+//        alert.addTextField { (TextField3) in
+//        }
+//        let OK = UIAlertAction(title: "Go", style: .destructive) { (OK) in
+//            let item = Product(image: UIImage(), price: Double(alert.textFields![0].text!)!, name: alert.textFields![1].text!, url: alert.textFields![2].text!)
+//            //            self.arrayOfProducts.append(item)
+//            self.resultTableView.reloadData()
+//        }
+//        let cancel = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+//        
+//        alert.addAction(OK)
+//        alert.addAction(cancel)
+//        present(alert, animated: true, completion: nil)
     }
     
     func gimmeeBestBuy() {
