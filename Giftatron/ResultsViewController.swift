0c7c7bb948cfee54adaa2add22829dab5608dc1e
Bob Brown <bob.brown@d214.org>
2020-05-23 10:20:43 -0500

    Added code

--- Giftatron/ResultsViewController.swift
+++ Giftatron/ResultsViewController.swift
@@ -78,17 +78,17 @@ class ResultsViewController: UIViewController, UITableViewDataSource,UITableView
         let cellTitle = tableView.cellForRow(at: indexPath)?.textLabel?.text!
         var gift = Gift(name: "", salePrice: 0, mobileUrl: URL(string: "http://google.com")!, image: "", longDescription: "")
         for x in arrayOfProducts {
-                   if cellTitle == x.name {
-                    gift = x
-                       break
-                   }
-               }
+            if cellTitle == x.name {
+                gift = x
+                break
+            }
+        }
         let alert = UIAlertController(title: gift.name, message: "\(gift.mobileUrl)" + "\n" + "\(gift.salePrice)" + "\n" + "\((gift.longDescription) ?? " ")", preferredStyle: .alert)
         let ok = UIAlertAction(title: "OK",style: .default,handler: nil)
         alert.addAction(ok)
         present(alert, animated: true)
         
-
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -137,14 +137,14 @@ class ResultsViewController: UIViewController, UITableViewDataSource,UITableView
                     highestPoint = x
                 }
             }
-
+            
             
             for y in arrayOrganizeOfPrices {
                 if y < highestPoint && y > secondHighestPoint {
                     secondHighestPoint = y
                 }
             }
-        
+            
             for z in arrayOrganizeOfPrices {
                 if z < secondHighestPoint && z > thirdHighestPoint
                 {
@@ -152,14 +152,14 @@ class ResultsViewController: UIViewController, UITableViewDataSource,UITableView
                 }
                 
             }
-        
+            
             
             for f in arrayOrganizeOfPrices {
                 if f < thirdHighestPoint && f > fourthHighestPoint {
                     fourthHighestPoint = f
                 }
             }
-        
+            
             
             for g in arrayOrganizeOfPrices {
                 if g < fourthHighestPoint && g > fifthHighestPoint {
@@ -171,7 +171,7 @@ class ResultsViewController: UIViewController, UITableViewDataSource,UITableView
                 if s <= fifthHighestPoint && s > sixthHighestPoint {
                     sixthHighestPoint = s
                 }
-               
+                
             }
             for v in arrayOrganizeOfPrices {
                 if v <= sixthHighestPoint && v > seventhHighestPoint {
@@ -183,7 +183,7 @@ class ResultsViewController: UIViewController, UITableViewDataSource,UITableView
                 if e <= seventhHighestPoint && e > eighthHighestPoint {
                     eighthHighestPoint = e
                 }
-        
+                
             }
             for n in arrayOrganizeOfPrices {
                 if n <= eighthHighestPoint && n > ninthHighestPoint {
@@ -196,12 +196,12 @@ class ResultsViewController: UIViewController, UITableViewDataSource,UITableView
                     lowestPoint = l
                 }
             }
-   // _____________________________________________________________________________________
+            // _____________________________________________________________________________________
             
-        for x in arrayOrganizeOfPrices {
-            if x == highestPoint {
-                arraySortOfPrices.append(x)
-            }
+            for x in arrayOrganizeOfPrices {
+                if x == highestPoint {
+                    arraySortOfPrices.append(x)
+                }
             }
             for y in arrayOrganizeOfPrices {
                 if y == secondHighestPoint {
@@ -256,45 +256,71 @@ class ResultsViewController: UIViewController, UITableViewDataSource,UITableView
             priceNine = arraySortOfPrices[8]
             priceTen =  arraySortOfPrices[9]
             for gift in arrayOfProducts {
-            
-            if gift.salePrice == priceOne //&& gift.salePrice != secondHighestPoint
-            {
-                newArrayOfProducts.append(gift)
+                
+                if gift.salePrice == priceOne //&& gift.salePrice != secondHighestPoint
+                {
+                    newArrayOfProducts.append(gift)
+                }
             }
             
-            if gift.salePrice == priceTwo &&
-                priceTwo != priceOne {
-                newArrayOfProducts.append(gift)
-            } else  {
-                print("")
+            for gift in arrayOfProducts {
+                if gift.salePrice == priceTwo &&
+                    priceTwo != priceOne {
+                    newArrayOfProducts.append(gift)
+                } else  {
+                    print("")
                 }
-                
-            if gift.salePrice == priceThree && priceThree != priceTwo && priceThree != priceOne{
-                newArrayOfProducts.append(gift)
-            } else {
-                
+            }
+            
+            for gift in arrayOfProducts {
+                if gift.salePrice == priceThree && priceThree != priceTwo && priceThree != priceOne{
+                    newArrayOfProducts.append(gift)
+                } else {
+                    
                 }
+            }
+            
+            for gift in arrayOfProducts {
                 if gift.salePrice == priceFour && priceFour != priceThree && priceFour != priceTwo && priceFour != priceOne{
-                newArrayOfProducts.append(gift)
+                    newArrayOfProducts.append(gift)
+                }
             }
+            
+            for gift in arrayOfProducts {
                 if gift.salePrice == priceFive && priceFive != priceFour && priceFive != priceThree && priceFive != priceTwo && priceFive != priceOne {
-                newArrayOfProducts.append(gift)
+                    newArrayOfProducts.append(gift)
+                }
             }
-            if gift.salePrice == priceSix && priceSix != priceFive && priceSix != priceFour && priceSix != priceThree && priceSix != priceTwo && priceSix != priceOne{
-                newArrayOfProducts.append(gift)
+            
+            for gift in arrayOfProducts {
+                if gift.salePrice == priceSix && priceSix != priceFive && priceSix != priceFour && priceSix != priceThree && priceSix != priceTwo && priceSix != priceOne{
+                    newArrayOfProducts.append(gift)
+                }
             }
-            if gift.salePrice == priceSeven && priceSeven != priceSix && priceSeven !=  priceFive && priceSeven != priceFour && priceSeven != priceThree && priceSeven != priceTwo && priceSeven != priceOne{
-                newArrayOfProducts.append(gift)
+            
+            for gift in arrayOfProducts {
+                if gift.salePrice == priceSeven && priceSeven != priceSix && priceSeven !=  priceFive && priceSeven != priceFour && priceSeven != priceThree && priceSeven != priceTwo && priceSeven != priceOne{
+                    newArrayOfProducts.append(gift)
+                }
             }
-            if gift.salePrice == priceEight && priceEight != priceSeven && priceEight != priceSix && priceEight != priceFive && priceEight != priceFour && priceEight != priceThree && priceEight != priceTwo && priceEight != priceOne {
-                newArrayOfProducts.append(gift)
+            
+            
+            for gift in arrayOfProducts {
+                if gift.salePrice == priceEight && priceEight != priceSeven && priceEight != priceSix && priceEight != priceFive && priceEight != priceFour && priceEight != priceThree && priceEight != priceTwo && priceEight != priceOne {
+                    newArrayOfProducts.append(gift)
+                }
             }
-            if gift.salePrice == priceNine && priceNine != priceEight && priceNine != priceSeven && priceNine != priceSix && priceNine != priceFive && priceNine != priceFour && priceNine != priceThree && priceNine != priceTwo && priceNine != priceOne {
-                newArrayOfProducts.append(gift)
+            
+            for gift in arrayOfProducts {
+                if gift.salePrice == priceNine && priceNine != priceEight && priceNine != priceSeven && priceNine != priceSix && priceNine != priceFive && priceNine != priceFour && priceNine != priceThree && priceNine != priceTwo && priceNine != priceOne {
+                    newArrayOfProducts.append(gift)
+                }
             }
-            if gift.salePrice == priceTen && priceTen < priceNine {
-                newArrayOfProducts.append(gift)
-            }
+            
+            for gift in arrayOfProducts {
+                if gift.salePrice == priceTen && priceTen < priceNine {
+                    newArrayOfProducts.append(gift)
+                }
                 
             }
             arrayOfProducts = newArrayOfProducts
@@ -309,26 +335,25 @@ class ResultsViewController: UIViewController, UITableViewDataSource,UITableView
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if product >= 0 && product <= 9 {
             
-        if let cell = resultTableView.dequeueReusableCell(withIdentifier: "cell") { //*//
-            
-            cell.textLabel?.text = arrayOfProducts[indexPath.row].name
-            var url = URL(string: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png")
-            if let image = arrayOfProducts[indexPath.row].image {
-                url = URL(string: image)
-            }
-            print(url)
-            cell.imageView?.image = UIImage(ciImage: CIImage(contentsOf: url!)!)
-            cell.detailTextLabel?.text = "$" + String(arrayOfProducts[indexPath.row].salePrice) + "    " + "\(arrayOfProducts[indexPath.row].mobileUrl)"
-            
-            price = "\(arrayOfProducts[indexPath.row].salePrice)"
-            
-            product += 1
-            return cell
-        } else {
-            return UITableViewCell()
+            if let cell = resultTableView.dequeueReusableCell(withIdentifier: "cell") { //*//
+                
+                cell.textLabel?.text = arrayOfProducts[indexPath.row].name
+                var url = URL(string: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png")
+                if let image = arrayOfProducts[indexPath.row].image {
+                    url = URL(string: image)
+                }
+                print(url)
+                cell.imageView?.image = UIImage(ciImage: CIImage(contentsOf: url!)!)
+                cell.detailTextLabel?.text = "$" + String(arrayOfProducts[indexPath.row].salePrice) + "    " + "\(arrayOfProducts[indexPath.row].mobileUrl)"
+                
+                price = "\(arrayOfProducts[indexPath.row].salePrice)"
+                
+                product += 1
+                return cell
+            } else {
+                return UITableViewCell()
             }
         }
-        ///
             
             
             
@@ -344,39 +369,39 @@ class ResultsViewController: UIViewController, UITableViewDataSource,UITableView
                 if let image = arrayOfProducts[indexPath.row].image {
                     url = URL(string: image)
                 }
-//                print(url)
+                //                print(url)
                 cell.imageView?.image = UIImage(ciImage: CIImage(contentsOf: url!)!)
                 cell.detailTextLabel?.text = "$" + String(arrayOfProducts[indexPath.row].salePrice) + "    " + "\(arrayOfProducts[indexPath.row].mobileUrl)"
                 product += 1
                 return cell
             } else {
                 return UITableViewCell()
-        }
+            }
         }
     }
     func addNewItem() {
         arrayOfAnswers.removeAll()
         resultTableView.reloadData()
         gimmeeBestBuy()
-//        let alert = UIAlertController(title: "New Item", message: "enter your item name below", preferredStyle: .alert)
-//        alert.addTextField { (TextField) in
-//            TextField.placeholder = "Price"
-//        }
-//        alert.addTextField { (TextField2) in
-//            TextField2.placeholder = "Name"
-//        }
-//        alert.addTextField { (TextField3) in
-//        }
-//        let OK = UIAlertAction(title: "Go", style: .destructive) { (OK) in
-//            let item = Product(image: UIImage(), price: Double(alert.textFields![0].text!)!, name: alert.textFields![1].text!, url: alert.textFields![2].text!)
-//            //            self.arrayOfProducts.append(item)
-//            self.resultTableView.reloadData()
-//        }
-//        let cancel = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
-//        
-//        alert.addAction(OK)
-//        alert.addAction(cancel)
-//        present(alert, animated: true, completion: nil)
+        //        let alert = UIAlertController(title: "New Item", message: "enter your item name below", preferredStyle: .alert)
+        //        alert.addTextField { (TextField) in
+        //            TextField.placeholder = "Price"
+        //        }
+        //        alert.addTextField { (TextField2) in
+        //            TextField2.placeholder = "Name"
+        //        }
+        //        alert.addTextField { (TextField3) in
+        //        }
+        //        let OK = UIAlertAction(title: "Go", style: .destructive) { (OK) in
+        //            let item = Product(image: UIImage(), price: Double(alert.textFields![0].text!)!, name: alert.textFields![1].text!, url: alert.textFields![2].text!)
+        //            //            self.arrayOfProducts.append(item)
+        //            self.resultTableView.reloadData()
+        //        }
+        //        let cancel = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+        //
+        //        alert.addAction(OK)
+        //        alert.addAction(cancel)
+        //        present(alert, animated: true, completion: nil)
     }
     
     func gimmeeBestBuy() {
@@ -398,14 +423,14 @@ class ResultsViewController: UIViewController, UITableViewDataSource,UITableView
             array = smoob.components(separatedBy: " ")
         }
         smood = "("
-//        print(array)
+        //        print(array)
         for x in array {
             if x != "" {
                 smood += "search=\(x)&"
-//                print(smood)
+                //                print(smood)
             }
         }
-//        print(smood)
+        //        print(smood)
         smood += calculatePrice(answer3)
         if ans4 != "" {
             if let numb = Int(ans4) {
@@ -418,10 +443,10 @@ class ResultsViewController: UIViewController, UITableViewDataSource,UITableView
         smood += ")"
         print(smood)
         let string = "https://api.bestbuy.com/v1/products\(smood)?format=json&pageSize=10&apiKey=GVfY17LDc7x2vt4lBya2D26z"
-//        print(string)
+        //        print(string)
         let str =   string.addingPercentEncoding(withAllowedCharacters:CharacterSet.urlQueryAllowed)
         url = URL(string: str!)
-//        print(url?.absoluteString)
+        //        print(url?.absoluteString)
         
         URLSession.shared.dataTask(with: url!) { (data, response, error) in
             //            print(data)
@@ -431,7 +456,7 @@ class ResultsViewController: UIViewController, UITableViewDataSource,UITableView
                     guard let product = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) else { return }
                     let dictionary = product as! NSDictionary
                     let myProducts = dictionary["products"] as! NSArray
-//                    print(dictionary["products"])
+                    //                    print(dictionary["products"])
                     for x in myProducts {
                         let x = x as! NSDictionary
                         let name = x["name"] as! String
@@ -441,7 +466,7 @@ class ResultsViewController: UIViewController, UITableViewDataSource,UITableView
                         let url = x["mobileUrl"] as! String
                         self.arrayOfProducts.append(Gift(name: name, salePrice: salePrice, mobileUrl: URL(string: url)!, image: image, longDescription: longdes))
                         
-//                        print(name + " " + String(salePrice))
+                        //                        print(name + " " + String(salePrice))
                     }
                     
                     DispatchQueue.main.async {
@@ -475,14 +500,14 @@ class ResultsViewController: UIViewController, UITableViewDataSource,UITableView
     func calculatePrice(_ price : String) -> String {
         var string = ""
         let array = price.components(separatedBy: "-")
-//        print(array)
+        //        print(array)
         let more = array[0]
         string += "salePrice>\(more)&"
         let less = array[1]
         if less != "nada" {
             string += "salePrice<\(less)&"
         }
-//        print(string)
+        //        print(string)
         return string
     }
 }
